6e3d48ece05789db0e10caf84328c6f04f69afe9
John YS <johnys@stanford.edu>
2015-05-04 22:16:26 -0700

    layout profile

---
+++ Twitter/ProfileHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileHeaderCell.swift
+//  Twitter
+//
+//  Created by John YS on 5/4/15.
+//  Copyright (c) 2015 Silicon Valley Insight. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
