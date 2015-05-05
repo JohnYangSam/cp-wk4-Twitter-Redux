89979ad64aec90fb717f322a3e166124613fb0f2
John YS <johnys@stanford.edu>
2015-05-05 00:22:46 -0700

    basic menu cells compiling with text

---
+++ Twitter/MenuCell.swift
@@ -0,0 +1,26 @@
+//
+//  MenuCell.swift
+//  Twitter
+//
+//  Created by John YS on 5/5/15.
+//  Copyright (c) 2015 Silicon Valley Insight. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UITableViewCell {
+
+    @IBOutlet weak var menuLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Twitter/SidePanelViewController.swift
@@ -0,0 +1,70 @@
+//
+//  SidePanelViewController.swift
+//  Twitter
+//
+//  Created by John YS on 5/5/15.
+//  Copyright (c) 2015 Silicon Valley Insight. All rights reserved.
+//
+
+import UIKit
+
+class SidePanelViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.reloadData()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell: MenuCell = tableView.dequeueReusableCellWithIdentifier("MenuCell", forIndexPath: indexPath) as! MenuCell
+        
+        switch indexPath.row {
+        case 0:
+            cell.menuLabel.text = "Profile"
+            break
+        case 1:
+            cell.menuLabel.text = "Timeline"
+            break
+        case 2:
+            cell.menuLabel.text = "Mentions"
+            break
+        default:
+            break
+            
+        }
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
